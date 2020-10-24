5a6e7dbafc9ca34960d3ccf00388b6e27d72da43
jonas.mouret <jonas@Jonas-PC.localdomain>
2020-10-24 18:03:32 +0200

    Add order fonctionnality

--- alpifoodapp/admin.py
+++ alpifoodapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from alpifoodapp.models import Restaurant, Customer, Driver, Meal
+from alpifoodapp.models import Restaurant, Customer, Driver, Meal, Order
 
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(Driver)
 admin.site.register(Meal)
+admin.site.register(Order) 
